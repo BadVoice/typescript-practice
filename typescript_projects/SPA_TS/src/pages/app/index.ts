bd1e54f1872f0f977264ce8310e312e468812c2b
Herman <smurovnemez@mail.ru>
2022-12-10 08:50:52 -0600

    feat: hash routing

--- typescript_projects/SPA_TS/src/pages/app/index.ts
+++ typescript_projects/SPA_TS/src/pages/app/index.ts
@@ -1,9 +1,36 @@
-import MainPage from '../main'
+import MainPage from '../main';
+import Page from '../../core/templates/page'
+import SettingsPage from '../settings/index';
 
 class App {
     private container: HTMLElement; // learn private 
     private initPage: MainPage;
 
+    static renderNewPage(idPage: string) {
+        document.body.innerHTML = ''
+        let page: Page | null = null;
+
+        if (idPage === 'main-page') {
+            page = new MainPage(idPage)
+        } else if (idPage === 'settings-page') {
+            page = new SettingsPage(idPage)
+        } else if (idPage === 'statistics-page') {
+            page = new SettingsPage(idPage)
+        }
+
+        if(page) {
+            const pageHTML = page.render();
+            document.body.append(pageHTML)
+        }
+    }
+
+    private enableRouteChange() {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            console.log('haschange', hash)
+            App.renderNewPage(hash);
+        })
+    }
 
     constructor() {
         this.container = document.body;
@@ -11,8 +38,8 @@ class App {
     }
 
     run() {
-        const mainPageHTML = this.initPage.render()
-        this.container.append(mainPageHTML)
+        App.renderNewPage('statistics-page');
+        this.enableRouteChange()
     }
 }
 
